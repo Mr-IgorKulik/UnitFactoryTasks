1ae88d2e5e3ffc14c4f407ee998ed011e30211b4
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-07 13:50:58 +0200

    t01 ---- mx_say_wake_up.c

---
+++ sprint01/t01/mx_say_wake_up.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void mx_say_wake_uo (void) {
+    printf("%s", "Wake up, NEO \\ (^_^) / ...\n");
+    printf("%s", "The Matrix has you ...\n");
+}
+
+/* int main () {
+    mx_say_wake_uo();
+} */
