1d8c6dceb43cba761d821c4913584c2f751fae88
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-07 13:58:42 +0200

    t02 ---- mx_write_knock_knock

---
+++ sprint01/t02/mx_write_knock_knock.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+
+void mx_write_knock_knock(void) {
+    const char str[]= "Follow the white rabbit.\nKnock, knock, Neo.\n";
+    write(1, str, strlen(str));
+}
+
+int main () {
+    mx_write_knock_knock();
+    return 0;
+}
