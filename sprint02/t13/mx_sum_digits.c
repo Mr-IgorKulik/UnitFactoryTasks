0bec2664e80505c016df54d3f42c616186e4fd28
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-11 14:53:42 +0200

    t13 ---- mx_sum_digits

---
+++ sprint02/t13/mx_sum_digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int mx_sum_digits(int num) {
+    int res = 0;
+    if (num < 0) num = 0 - num;
+    while (num%10 > 0)
+    {
+        res += num%10;
+        num /= 10;
+    }
+    return res;
+}
+
+int main() {
+    printf("%d\n", mx_sum_digits(435));
+    printf("%d\n", mx_sum_digits(-555));
+}
