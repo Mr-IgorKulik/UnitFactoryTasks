7dc243337cc6217364b763a11c82f83f1b787934
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-16 20:46:57 +0200

    t09 ---- mx_strncmp

---
+++ sprint04/t09/mx_strncmp.c
@@ -0,0 +1,25 @@
+/* #include <stdio.h> */
+
+int mx_strncmp(const char *s1, const char *s2, int n) {
+    int count = 0;
+    while (s1[count] && s2[count] && s1[count] == s2[count] && count < n)
+    {
+        count++;
+    }
+
+    if (count == 0) {
+        return s1[0] - s2[0];
+    } else if (s1[count + 1] == s2[count + 1] && s1[count + 1] == '\0' && s2[count + 1] == '\0') {
+        return 0;
+    } else
+    {
+        return s1[count] - s2[count];
+    }
+    
+}
+
+/* int main() {
+    char str1[] = "Hello! It's me!";
+    char str2[] = "Hello!";
+    printf("%d\n", mx_strncmp(str1, str2, 8));
+} */
