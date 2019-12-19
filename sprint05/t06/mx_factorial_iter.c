880e968e3b199b82dd101f5c10435c038e3bb699
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-19 23:05:11 +0200

    t06 ---- mx_factorial_iter

---
+++ sprint05/t06/mx_factorial_iter.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int mx_factorial_iter(int n) {
+    int res = 1;
+    if(n == 0) return 1;
+    if(n < 0) return 0;
+    for (int i = 1; i <= n; i++) {
+        if(res <= 2147483647 && n > 0) {
+            res *= i;
+        }else
+        {
+            return 0;
+        }
+        
+    }
+    return res;
+}
+
+int main() {
+    printf("%d\n", mx_factorial_iter(0));
+    printf("%d\n", mx_factorial_iter(2));
+    printf("%d\n", mx_factorial_iter(10));
+    printf("%d\n", mx_factorial_iter(-1));
+}
