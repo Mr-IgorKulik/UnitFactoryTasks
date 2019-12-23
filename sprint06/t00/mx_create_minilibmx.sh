63251dbca58ebcb7a00d4639574eb29ad527d0bb
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-23 19:28:01 +0200

    t00 ---- mx_create_minilibmx.sh

--- sprint06/t00/mx_create_minilibmx.sh
+++ sprint06/t00/mx_create_minilibmx.sh
@@ -1,1 +1,11 @@
-ar rc minilibmx.a mx_atoi.c mx_isdigit.c mx_isspace.c mx_printchar.c mx_printint.c mx_printstr.c mx_strcmp.c mx_strcpy.c mx_strlen.c 
+clang -c mx_isspace.c -o mx_isspace.o
+clang -c  mx_isdigit.c -o mx_isdigit.o
+clang -c mx_atoi.c -o mx_atoi.o
+clang -c mx_printchar.c -o mx_printchar.o
+clang -c mx_printint.c -o mx_printint.o
+clang -c mx_printstr.c -o mx_printstr.o
+clang -c mx_strcpy.c -o mx_strcpy.o
+clang -c  mx_strlen.c -o mx_strlen.o
+clang -c mx_strcmp.c -o mx_strcmp.o
+ar rc minilibmx.a mx_atoi.o mx_isdigit.o mx_isspace.o mx_printchar.o mx_printint.o mx_printstr.o mx_strcmp.o mx_strcpy.o mx_strlen.o
+ranlib minilibmx.a
