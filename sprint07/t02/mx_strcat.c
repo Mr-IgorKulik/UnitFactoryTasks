1ccb35c4384049313f51ad098ca2879ac6b76463
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-26 01:12:02 +0200

    t02 ---- mx_strjoin

---
+++ sprint07/t02/mx_strcat.c
@@ -0,0 +1,18 @@
+/* #include <stdio.h> */
+
+char *mx_strcat(char *s1, const char *s2) {
+    int len = mx_strlen(s1) + mx_strlen(s2);
+
+    for (int i = mx_strlen(s1), j = 0; i < len; i++, j++) {
+        s1[i] = s2[j];
+    }
+
+    return s1;
+}
+
+/* int main() {
+    char str[] = "Hello!";
+    char str2[] = "It's me!";
+
+    printf("%s\n", mx_strcat(str, str2));
+} */
