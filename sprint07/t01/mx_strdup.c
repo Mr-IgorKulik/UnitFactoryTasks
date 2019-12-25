5ac81dbbd13e051967681da5f3ef147dae70164d
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2019-12-26 00:42:53 +0200

    t01 ---- mx_strdup

--- sprint07/t01/mx_strdup.c
+++ sprint07/t01/mx_strdup.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h> */
 #include "mx_strlen.c"
 #include "mx_strcpy.c"
 #include "mx_strnew.c"
@@ -11,11 +11,11 @@ char *mx_strdup(const char *str) {
     return resStr;
 }
 
-int main() {
+/* int main() {
     char str1[] = "Hello";
     printf("%s\n", str1);
     char *str2 = NULL;
     printf("%s\n", str2);
     str2 = mx_strdup(str1);
     printf("%s\n", str2);
-}
+} */
